a17995eef5361fe10fa69c8a5e526ccb99948511
chrisdev12 <christianm.bravop@gmail.com>
2020-02-18 10:04:08 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+
+
+?>
+
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./Styles/styles.css">
+    <title>Libreria Virtual</title>
+</head>
+
+<body>
+    <nav>
+    </nav>
+    <main>
+
+    </main>
+    <section>
+
+    </section>
+    <form method="POST">
+
+    </form>
+    <footer>
+
+    </footer>
+</body>
+
+</html>
