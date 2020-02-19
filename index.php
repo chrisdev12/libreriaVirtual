054878114353a0e9e4aac88ee08174cbbfb70fa6
camilo <camilo7davila@gmail.com>
2020-02-19 09:18:58 -0500

    se agrego bootstrap

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ $prueba = $database->conectar();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./styles/index/styles.css">
+    <link rel="stylesheet" href="styles/bootstrap/bootstrap.min.css">
     <title>Libreria Virtual</title>
 </head>
 
@@ -28,6 +28,9 @@ $prueba = $database->conectar();
     <footer>
 
     </footer>
+
+    <script src="js/bootstrap/bootstrap.min.js"></script>
+    <script src="js/bootstrap/popper.min.js"></script>
 </body>
 
 </html>
