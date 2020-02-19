cc8c7207f600a3ae3882903f7c96c750e675d5d5
Blo2SonNT <migustanieto.2@gmail.com>
2020-02-19 09:24:09 -0500

    Holi

--- includes/class/class_user.php
+++ includes/class/class_user.php
@@ -35,6 +35,8 @@ class user extends Database {
         }
     }
 
+    // holi
+
     public function setUser($user) {
         $query = $this->connect_user_session()->prepare('SELECT * FROM tb_usuario WHERE correo = :user');
         $query->execute(['user' => $user]);
