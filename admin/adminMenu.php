f12919643d0273ebac5021581c456758552c91f1
Nicolas Martin <nicolasfelipemartin@hotmail.com>
2020-02-20 11:13:53 -0500

    Se agrego carpeta admin y su menu

---
+++ admin/adminMenu.php
@@ -0,0 +1,45 @@
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../styles/bootstrap/bootstrap.min.css">
+        
+        <title>Menu administrador</title>
+    </head>
+    <body>
+        <?php
+        include('../secciones/navbar.php');
+        ?>
+        <div class="container mt-3">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="opcion_menu">
+                        <h2>Autores</h2>
+                        <p>Aquí encontraras toda la información y utilidad sobre los autores</p>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="opcion_menu">
+                        <h2>Libros</h2>
+                        <p>Aquí encontraras toda la información y utilidad sobre los autores</p>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="opcion_menu">
+                        <h2>Categorias</h2>
+                        <p>Aquí encontraras toda la información y utilidad sobre los autores</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+        <script src="../js/bootstrap/bootstrap.min.js"></script>
+        <script src="../js/bootstrap/popper.min.js"></script>
+        
+    </body>
+    </html>
